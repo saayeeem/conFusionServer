2c34a45687aa93387e5c6c098a637e95548f8d04
saayeeem <chysayem9@gmail.com>
2021-09-28 21:47:23 +0600

    Passport

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const connect = mongoose.connect(url);
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
@@ -49,24 +52,21 @@ app.use(
     store: new FileStore(),
   })
 );
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
