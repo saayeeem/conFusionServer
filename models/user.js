89a36d836fc5b6ccbc71cc05c60099caa8a9ed3f
saayeeem <chysayem9@gmail.com>
2021-10-01 23:35:35 +0600

    mongoose population

--- models/user.js
+++ models/user.js
@@ -3,6 +3,16 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var User = new Schema({
+
+  //username and password will store by liabry
+  firstname: {
+    type: String,
+    default: "",
+  },
+  lastname: {
+    type: String,
+    default: "",
+  },
   admin: {
     type: Boolean,
     default: false,
