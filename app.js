837ed4a7056620f7274c451e006cc66d4f7ac038
saayeeem <chysayem9@gmail.com>
2021-10-05 22:40:29 +0600

    the problem was in app.js :angry:

--- app.js
+++ app.js
@@ -59,7 +59,8 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use(express.static(__dirname + "../public"));
+// app.use(express.static(__dirname + "../public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
